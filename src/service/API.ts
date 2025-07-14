e0c68ae459699b24f1bc4b2d0777ca6be88fe2f0
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-14 13:09:23 -0300

    fix: url da api corrigida

--- src/service/API.ts
+++ src/service/API.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://localhost:8080";
+const API_BASE_URL = "http://localhost:8080";
 
 export const API = axios.create({
   baseURL: API_BASE_URL,
