a86dc980909214249f3f6a8437f73445078192d2
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-15 16:59:13 -0300

    fiz: agora retorna corretamente

--- src/components/button/PrimaryButton.tsx
+++ src/components/button/PrimaryButton.tsx
@@ -4,8 +4,10 @@ interface PrimaryButtonProps {
 }
 
 export function PrimaryButton(props: PrimaryButtonProps) {
-    <button
-        className="w-full mt-5 bg-primary rounded-xl p-3 font-bold text-white text-[20px] shadow cursor-pointer hover:bg-red-700 duration-500 ease-in-out"
-        onClick={props.onClick} type="button"
-    >{props.label}</button>
+    return (
+        <button
+            className="w-full mt-5 bg-primary rounded-xl p-2 font-semibold text-center text-white text-[20px] shadow cursor-pointer hover:bg-red-800 duration-400 ease-in-out"
+            onClick={props.onClick}
+        >{props.label}</button>
+    );
 }
