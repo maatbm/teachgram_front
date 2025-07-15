fbbcc83c4a22829fab0e680dc1bebd62a59994eb
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-15 08:46:59 -0300

    add: componente de sideImage

---
+++ src/components/SideImage.tsx
@@ -0,0 +1,9 @@
+import sideImage from "@componentsAssets/SideImage/sideImage.png";
+
+export function SideImage() {
+    return (
+        <div className="w-full h-full flex items-center justify-center">
+            <img src={sideImage} alt="Right side image" className="w-full h-full" />
+        </div>
+    );
+}
