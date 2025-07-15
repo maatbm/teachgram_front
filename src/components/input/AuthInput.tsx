38224b514483ab4b22e2adfda32cb07879e5a616
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-15 09:34:12 -0300

    fix: otimizações

--- src/components/input/AuthInput.tsx
+++ src/components/input/AuthInput.tsx
@@ -1,22 +1,22 @@
 interface AuthInputProps {
     label: string;
-    labelColor?: string;
+    labelColor: string;
     type: string;
     placeholder?: string;
     required?: boolean;
-    onchange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export function AuthInput(props: AuthInputProps) {
     return (
         <div className="w-full mb-4">
-            <label className={`block mb-1 ${props.labelColor || "text-terciary"}`}>{props.label}</label>
+            <label className={`block mb-1 ${props.labelColor}`}>{props.label}</label>
             <input
                 type={props.type}
                 placeholder={props.placeholder}
                 required={props.required}
-                onChange={props.onchange}
-                className="w-full p-2 border-1 border-[#A09F9F] rounded-[8px] outline-primary"
+                onChange={props.onChange}
+                className="w-full p-2 border border-secondary rounded-[8px] outline-primary"
             />
         </div>
     );
