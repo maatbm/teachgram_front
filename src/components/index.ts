d68ac6b3d8685466251431a45c2627da476226bc
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-15 14:38:03 -0300

    add: loading

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { PrimaryButton } from './button/PrimaryButton';
 export { SideImage } from './image/SideImage';
 export { TopImage } from './image/TopImage';
 export { AuthInput } from './input/AuthInput';
+export { Loading } from './loading/Loading';
