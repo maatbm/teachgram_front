e28d246bd50edcd373b6e3e8b23754f3e4a8c456
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-15 11:33:17 -0300

    fix: hover mudado

--- src/components/button/PrimaryButton.tsx
+++ src/components/button/PrimaryButton.tsx
@@ -5,7 +5,7 @@ interface PrimaryButtonProps {
 
 export function PrimaryButton(props: PrimaryButtonProps) {
     <button
-        className="w-full mt-5 bg-primary rounded-xl p-3 font-bold text-white text-[20px] shadow cursor-pointer hover:scale-[1.1] duration-400"
+        className="w-full mt-5 bg-primary rounded-xl p-3 font-bold text-white text-[20px] shadow cursor-pointer hover:bg-red-700 duration-50 ease-in-out"
         onClick={props.onClick} type="button"
     >{props.label}</button>
 }
