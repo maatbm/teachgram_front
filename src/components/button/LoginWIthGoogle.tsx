302801b1eebe3874ad1efe9c53987b985fbe89cf
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-15 11:43:36 -0300

    add: botão de login with google

---
+++ src/components/button/LoginWIthGoogle.tsx
@@ -0,0 +1,14 @@
+import googleIcon from "@componentsAssets/LoginWithGoogle/googleLogo.png";
+
+export function LoginWithGoogle() {
+    return (
+        <div
+            className="w-full flex justify-center items-center gap-6 p-4 rounded-xl shadow mb-6 cursor-pointer hover:scale-[1.1] duration-400"
+            role="button"
+            onClick={() => { console.log('Login with Google clicked') }}
+        >
+            <img src={googleIcon} alt="Google icon" className="w-[20px]" />
+            <span className="text-lg font-semibold">Login with Google</span>
+        </div>
+    );
+}
