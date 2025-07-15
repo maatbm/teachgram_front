3df90c1f8ea003809d3c31361a0a0637c4e88e2c
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-15 14:18:23 -0300

    add: index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { LoginWithGoogleOrApple } from './LoginWithGoogleOrApple';
+export { PrimaryButton } from './button/PrimaryButton';
+export { SideImage } from './image/SideImage';
+export { TopImage } from './image/TopImage';
+export { AuthInput } from './input/AuthInput';
