ba78951765aba8ba7360ce697590318be1fbf73c
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-15 11:49:19 -0300

    otimizações

--- src/components/button/LoginWIthGoogle.tsx
+++ src/components/button/LoginWIthGoogle.tsx
@@ -3,12 +3,12 @@ import googleIcon from "@componentsAssets/LoginWithGoogle/googleLogo.png";
 export function LoginWithGoogle() {
     return (
         <div
-            className="w-full flex justify-center items-center gap-6 p-4 rounded-xl shadow mb-6 cursor-pointer hover:scale-[1.1] duration-400"
+            className="w-full flex justify-center items-center gap-6 p-4 rounded-xl shadow mb-6 cursor-pointer hover:bg-neutral-400 duration-500 ease-in-out"
             role="button"
             onClick={() => { console.log('Login with Google clicked') }}
         >
             <img src={googleIcon} alt="Google icon" className="w-[17px] h-[18px]" />
-            <span className="text-lg font-semibold">Login with Google</span>
+            <span className="text-lg font-semibold text-secondary">Login with Google</span>
         </div>
     );
 }
