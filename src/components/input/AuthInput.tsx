feafe54054576e262728114e8731725a358eec26
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-15 16:59:22 -0300

    fix: mudanças de tezto

--- src/components/input/AuthInput.tsx
+++ src/components/input/AuthInput.tsx
@@ -10,13 +10,13 @@ interface AuthInputProps {
 export function AuthInput(props: AuthInputProps) {
     return (
         <div className="w-full mb-4">
-            <label className={`block mb-1 ${props.labelColor}`}>{props.label}</label>
+            <label className={`block mb-1 text-[15px] ${props.labelColor}`}>{props.label}</label>
             <input
                 type={props.type}
                 placeholder={props.placeholder}
                 required={props.required}
                 onChange={props.onChange}
-                className="w-full p-2 border border-secondary rounded-[8px] outline-primary"
+                className="w-full p-2 border border-secondary rounded-[8px] outline-primary placeholder:text-[15px] placeholder:text-secondary"
             />
         </div>
     );
