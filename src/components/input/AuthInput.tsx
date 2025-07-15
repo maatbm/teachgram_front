49c1a126bb389ee43681bd6bde624411153bb016
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-15 09:27:22 -0300

    add: authinput

---
+++ src/components/input/AuthInput.tsx
@@ -0,0 +1,23 @@
+interface AuthInputProps {
+    label: string;
+    labelColor?: string;
+    type: string;
+    placeholder?: string;
+    required?: boolean;
+    onchange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export function AuthInput(props: AuthInputProps) {
+    return (
+        <div className="w-full mb-4">
+            <label className={`block mb-1 ${props.labelColor || "text-terciary"}`}>{props.label}</label>
+            <input
+                type={props.type}
+                placeholder={props.placeholder}
+                required={props.required}
+                onChange={props.onchange}
+                className="w-full p-2 border-1 border-[#A09F9F] rounded-[8px] outline-primary"
+            />
+        </div>
+    );
+}
