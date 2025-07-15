ea58d842737920fce1bf98d403e0600232aa770e
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-15 14:08:19 -0300

    add: logar com google apple

---
+++ src/components/loginWIthGoogleOrApple/LoginWithGoogleOrApple.tsx
@@ -0,0 +1,14 @@
+import { LoginWithGoogle } from "@components/button/LoginWIthGoogle";
+import { LoginWithApple } from "@components/button/LoginWithApple";
+
+export function LoginWithGoogleOrApple() {
+    return (
+        <>
+            <fieldset className="w-full border-t-2 border-secondary text-secondary">
+                <legend className="text-center px-7 mb-5">Entrar com</legend>
+                <LoginWithGoogle />
+                <LoginWithApple />
+            </fieldset>
+        </>
+    );
+}
