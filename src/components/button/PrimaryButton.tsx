55628519b9778a4f1202810b5a50fa4c05960f2f
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-16 00:00:12 -0300

    add: disabled

--- src/components/button/PrimaryButton.tsx
+++ src/components/button/PrimaryButton.tsx
@@ -1,13 +1,14 @@
 interface PrimaryButtonProps {
     label: string;
-    onClick?: () => void;
+    disabled?: boolean;
 }
 
 export function PrimaryButton(props: PrimaryButtonProps) {
     return (
         <button
             className="w-full mt-5 bg-primary rounded-xl p-2 font-semibold text-center text-white text-[20px] shadow cursor-pointer hover:bg-red-800 duration-400 ease-in-out"
-            onClick={props.onClick}
+            disabled={props.disabled}
+            type="submit"
         >{props.label}</button>
     );
 }
