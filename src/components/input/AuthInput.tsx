80bd6a5c5b6f6f737132bb408e90fc4ab5fbcaed
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-16 18:12:09 -0300

    fix: margem corrigida

--- src/components/input/AuthInput.tsx
+++ src/components/input/AuthInput.tsx
@@ -11,7 +11,7 @@ interface AuthInputProps {
 
 export function AuthInput(props: AuthInputProps) {
     return (
-        <div className="w-full mb-3">
+        <div className="w-full mb-[9px]">
             <label className={`block mb-1 text-[15px] ${props.labelColor}`}>{props.label}</label>
             <input
                 type={props.type}
