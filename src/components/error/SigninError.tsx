89d3de06babbe23d6a0bafcedfac09de5f61808c
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-16 18:12:37 -0300

    fix: altura adicionada

--- src/components/error/SigninError.tsx
+++ src/components/error/SigninError.tsx
@@ -4,8 +4,8 @@ interface SigninErrorProps {
 
 export function SigninError(props: SigninErrorProps) {
     return (
-        <div className="w-full mt-5 flex justify-end items-center gap-2">
-            <div className="w-[4px] h-[4px] bg-primary p-2 rounded-circle"></div>
+        <div className="w-full flex justify-end items-center gap-2">
+            <div className="w-[12px] h-[12px] bg-primary rounded-circle"></div>
             <p className="font-semibold text-[15px] text-primary">{props.errorLabel}</p>
         </div>
     );
