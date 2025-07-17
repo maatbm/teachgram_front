24882e9d87e1bd8bbf0b68f2e698b8f97acc5021
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-17 09:22:12 -0300

    fix: sem required

--- src/components/signupForm/SignupFirstForm.tsx
+++ src/components/signupForm/SignupFirstForm.tsx
@@ -1,22 +1,20 @@
-import { AuthInput, PrimaryButton, SigninError } from "components";
+import { AuthInput } from "components";
 import type React from "react";
 import { type SignUpRequest } from "services/userService/user.types";
 
 interface SignupFirstFormProps {
     user: SignUpRequest;
     onChangeFunction: (e: React.ChangeEvent<HTMLInputElement>) => void
-    error: string;
 }
 
 export function SignupFirstForm(props: SignupFirstFormProps) {
     return (
-        <form className="w-full mt-2">
+        <div className="w-full">
             <AuthInput
                 label="Nome"
                 labelColor="text-quinary"
                 type="text"
                 placeholder="Digite seu nome"
-                required={true}
                 name="name"
                 value={props.user.name}
                 onChange={props.onChangeFunction}
@@ -26,7 +24,6 @@ export function SignupFirstForm(props: SignupFirstFormProps) {
                 labelColor="text-quinary"
                 type="email"
                 placeholder="Digite seu E-mail"
-                required={true}
                 name="mail"
                 value={props.user.mail}
                 onChange={props.onChangeFunction}
@@ -36,7 +33,6 @@ export function SignupFirstForm(props: SignupFirstFormProps) {
                 labelColor="text-quinary"
                 type="text"
                 placeholder="@ seu_username"
-                required={true}
                 name="username"
                 value={props.user.username}
                 onChange={props.onChangeFunction}
@@ -46,7 +42,6 @@ export function SignupFirstForm(props: SignupFirstFormProps) {
                 labelColor="text-quinary"
                 type="text"
                 placeholder="Faça uma descrição"
-                required={true}
                 name="description"
                 value={props.user.description}
                 onChange={props.onChangeFunction}
@@ -56,7 +51,6 @@ export function SignupFirstForm(props: SignupFirstFormProps) {
                 labelColor="text-quinary"
                 type="text"
                 placeholder="Digite seu número de celular"
-                required={true}
                 name="phone"
                 value={props.user.phone}
                 onChange={props.onChangeFunction}
@@ -66,13 +60,10 @@ export function SignupFirstForm(props: SignupFirstFormProps) {
                 labelColor="text-quinary"
                 type="password"
                 placeholder="Digite sua senha"
-                required={true}
                 name="password"
                 value={props.user.password}
                 onChange={props.onChangeFunction}
             />
-            {props.error && <SigninError errorLabel={props.error} />}
-            <PrimaryButton label="Próximo" />
-        </form>
+        </div>
     );
 }
