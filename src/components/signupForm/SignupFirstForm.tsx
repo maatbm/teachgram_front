426c163578fdf19b4017623e6ba03085cb471f4f
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-17 13:17:04 -0300

    add: componente de formulario

--- src/components/signupForm/SignupFirstForm.tsx
+++ src/components/signupForm/SignupFirstForm.tsx
@@ -9,61 +9,64 @@ interface SignupFirstFormProps {
 
 export function SignupFirstForm(props: SignupFirstFormProps) {
     return (
-        <div className="w-full">
-            <AuthInput
-                label="Nome"
-                labelColor="text-quinary"
-                type="text"
-                placeholder="Digite seu nome"
-                name="name"
-                value={props.user.name}
-                onChange={props.onChangeFunction}
-            />
-            <AuthInput
-                label="E-mail"
-                labelColor="text-quinary"
-                type="email"
-                placeholder="Digite seu E-mail"
-                name="mail"
-                value={props.user.mail}
-                onChange={props.onChangeFunction}
-            />
-            <AuthInput
-                label="Username"
-                labelColor="text-quinary"
-                type="text"
-                placeholder="@ seu_username"
-                name="username"
-                value={props.user.username}
-                onChange={props.onChangeFunction}
-            />
-            <AuthInput
-                label="Descrição"
-                labelColor="text-quinary"
-                type="text"
-                placeholder="Faça uma descrição"
-                name="description"
-                value={props.user.description}
-                onChange={props.onChangeFunction}
-            />
-            <AuthInput
-                label="Celular"
-                labelColor="text-quinary"
-                type="text"
-                placeholder="Digite seu número de celular"
-                name="phone"
-                value={props.user.phone}
-                onChange={props.onChangeFunction}
-            />
-            <AuthInput
-                label="Senha"
-                labelColor="text-quinary"
-                type="password"
-                placeholder="Digite sua senha"
-                name="password"
-                value={props.user.password}
-                onChange={props.onChangeFunction}
-            />
-        </div>
+        <>
+            <h2 className="text-quaternary font-semibold text-[20px]">Crie sua conta</h2>
+            <div className="w-full">
+                <AuthInput
+                    label="Nome"
+                    labelColor="text-quinary"
+                    type="text"
+                    placeholder="Digite seu nome"
+                    name="name"
+                    value={props.user.name}
+                    onChange={props.onChangeFunction}
+                />
+                <AuthInput
+                    label="E-mail"
+                    labelColor="text-quinary"
+                    type="email"
+                    placeholder="Digite seu E-mail"
+                    name="mail"
+                    value={props.user.mail}
+                    onChange={props.onChangeFunction}
+                />
+                <AuthInput
+                    label="Username"
+                    labelColor="text-quinary"
+                    type="text"
+                    placeholder="@ seu_username"
+                    name="username"
+                    value={props.user.username}
+                    onChange={props.onChangeFunction}
+                />
+                <AuthInput
+                    label="Descrição"
+                    labelColor="text-quinary"
+                    type="text"
+                    placeholder="Faça uma descrição"
+                    name="description"
+                    value={props.user.description}
+                    onChange={props.onChangeFunction}
+                />
+                <AuthInput
+                    label="Celular"
+                    labelColor="text-quinary"
+                    type="text"
+                    placeholder="Digite seu número de celular"
+                    name="phone"
+                    value={props.user.phone}
+                    onChange={props.onChangeFunction}
+                />
+                <AuthInput
+                    label="Senha"
+                    labelColor="text-quinary"
+                    type="password"
+                    placeholder="Digite sua senha"
+                    name="password"
+                    value={props.user.password}
+                    onChange={props.onChangeFunction}
+                />
+            </div>
+        </>
     );
 }
