f3507e8366667b2aae3b492f42bdc471155ec437
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-17 21:08:43 -0300

    fix: otmização

--- src/components/button/LoginWIthGoogle.tsx
+++ src/components/button/LoginWIthGoogle.tsx
@@ -3,7 +3,7 @@ import googleIcon from "assets/components/LoginWithGoogle/googleLogo.png";
 export function LoginWithGoogle() {
     return (
         <div
-            className="w-full flex justify-center items-center gap-6 p-2 rounded-xl shadow mb-6 cursor-pointer hover:bg-neutral-200 duration-500 ease-in-out"
+            className="w-full flex justify-center items-center gap-6 p-2 rounded-xl shadow mb-6 cursor-pointer hover-background-gray"
             role="button"
             onClick={() => { console.log('Login with Google clicked') }}
         >
--- src/components/button/LoginWithApple.tsx
+++ src/components/button/LoginWithApple.tsx
@@ -3,7 +3,7 @@ import appleIcon from "assets/components/LoginWithApple/appleLogo.png";
 export function LoginWithApple() {
     return (
         <div
-            className="w-full flex justify-center items-center gap-6 p-2 rounded-xl shadow mb-6 cursor-pointer hover:bg-neutral-200 duration-500 ease-in-out"
+            className="w-full flex justify-center items-center gap-6 p-2 rounded-xl shadow mb-6 cursor-pointer hover-background-gray"
             role="button"
             onClick={() => { console.log('Login with Apple clicked') }}
         >
