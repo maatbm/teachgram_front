9c7c855ff486ab1049d0295f7c265e78210cb9b7
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-19 09:58:34 -0300

    add: pagina inicial de configuração

---
+++ src/components/configComponent/ConfigComponent.tsx
@@ -0,0 +1,12 @@
+import { SideRight } from "components/sideRight/SideRight";
+
+export function ConfigComponent() {
+    return (
+        <div className="w-full h-full flex">
+            <div className="w-[82%] h-full"></div>
+            <div className="w-[18%] h-full">
+                <SideRight />
+            </div>
+        </div>
+    );
+}
